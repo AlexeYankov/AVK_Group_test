c3b16f549b4f9c221989826e3c1b9d805c8d5137
Alexey Yankov <yankovav.wm@gmail.com>
2024-08-02 12:10:19 +0300

    trash fix

--- src/features/mainPage/Main.tsx
+++ src/features/mainPage/Main.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import {useState} from "react";
-import {Center} from "@chakra-ui/react";
 import {SpinnerForClient} from "@/ui-kit/spinner";
 import {useGetPosts} from "@/api/postsApi";
 import {Footer} from "../footer";
@@ -12,21 +11,20 @@ const postsPerPage = 20;
 
 export const MainPage = () => {
     const [currentPage, setCurrentPage] = useState(1);
-    const [paymentsPerPage, setPaymentsPerPage] = useState(postsPerPage);
     const {data, isLoading} = useGetPosts();
     if (!data || isLoading) {
         return <SpinnerForClient />;
     }
-    const lastPaymentIndex = currentPage * paymentsPerPage;
-    const firstPaymentIndex = lastPaymentIndex - paymentsPerPage;
+    const lastPaymentIndex = currentPage * postsPerPage;
+    const firstPaymentIndex = lastPaymentIndex - postsPerPage;
     const currentData = data.length ? data.slice(firstPaymentIndex, lastPaymentIndex) : [];
     const paginate = (pageNumber: number) => {
         setCurrentPage(pageNumber);
     };
     return (
         <>
-            <Header />
             <Catalog posts={currentData} />
+            <Header />
             <Footer
                 postsPerPage={postsPerPage}
                 totalPosts={data.length ?? null}
