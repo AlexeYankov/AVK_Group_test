b4b1128ee99da39be4763b663ad0d6ed01b12a0a
Alexey Yankov <yankovav.wm@gmail.com>
2024-08-02 12:48:19 +0300

    fix items post container

--- src/features/post/post.tsx
+++ src/features/post/post.tsx
@@ -4,31 +4,43 @@ import Link from "next/link";
 import {CommentType, PostType} from "@/types/types";
 import {Box, Center, ListItem} from "@chakra-ui/react";
 
+const ListItemPage = (prop: {post: PostType | CommentType; isPost?: boolean}) => {
+    return (
+        <ListItem
+            p="4"
+            bgGradient="linear(gray.300, teal.400)"
+            rounded={"lg"}
+            mt="6"
+            minH={"80px"}
+            cursor={"pointer"}
+            _hover={{
+                shadow: "md",
+                transform: "translateY(-3px)",
+                transitionDuration: "0.2s",
+                transitionTimingFunction: "ease-in-out"
+            }}
+            color="black">
+            <Box>
+                {prop.isPost ? "post" : "comment"} id: {prop.post.id}
+            </Box>
+            {prop.post.title ?? prop.post.body}
+        </ListItem>
+    );
+};
+
 export const Post = (prop: {post: PostType | CommentType; isPost?: boolean}) => {
     const setPostInfo = () => localStorage.setItem("description", prop.post.body);
     return (
         <Center onClick={setPostInfo}>
-            <Link href={prop.isPost ? "/posts/" + prop.post.id : "#"} style={{width: "100%"}}>
-                <ListItem
-                    p="4"
-                    bgGradient="linear(gray.300, teal.400)"
-                    rounded={"lg"}
-                    mt="6"
-                    minH={"80px"}
-                    cursor={"pointer"}
-                    _hover={{
-                        shadow: "md",
-                        transform: "translateY(-3px)",
-                        transitionDuration: "0.2s",
-                        transitionTimingFunction: "ease-in-out"
-                    }}
-                    color="black">
-                    <Box>
-                        {prop.isPost ? "author" : "comment"} id: {prop.post.id}
-                    </Box>
-                    {prop.post.title ?? prop.post.body}
-                </ListItem>
-            </Link>
+            {prop.isPost ? (
+                <Link href={"/posts/" + prop.post.id} style={{width: "100%"}}>
+                    <ListItemPage post={prop.post} isPost={prop.isPost} />
+                </Link>
+            ) : (
+                <Box width="100%">
+                    <ListItemPage post={prop.post} isPost={prop.isPost} />
+                </Box>
+            )}
         </Center>
     );
 };
