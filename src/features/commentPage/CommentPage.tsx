344ab8df32cec8145c1f2d18d2901a36b54ad979
Alexey Yankov <yankovav.wm@gmail.com>
2024-08-05 13:53:23 +0300

    fix isNaN navigation

--- src/features/commentPage/CommentPage.tsx
+++ src/features/commentPage/CommentPage.tsx
@@ -1,18 +1,18 @@
 "use client";
 
-import { SpinnerForClient } from "@/ui-kit/spinner";
-import { Catalog } from "../catalog/catalog";
-import { useGetComments } from "@/api/commentsApi";
-import { useRouter } from "next/navigation";
+import {SpinnerForClient} from "@/ui-kit/spinner";
+import {Catalog} from "../catalog/catalog";
+import {useGetComments} from "@/api/commentsApi";
+import {useRouter} from "next/navigation";
 
-export const CommentPage = ({ id }: { id: string }) => {
-  const router = useRouter();
-  const { data, isLoading, isFetching } = useGetComments(id);
-  if (+id > 101) {
-    router.push("/posts");
-  }
-  if (!data || isLoading || isFetching) {
-    return <SpinnerForClient />;
-  }
-  return <Catalog posts={data} id={id} />;
+export const CommentPage = ({id}: {id: string}) => {
+    const router = useRouter();
+    const {data, isLoading, isFetching} = useGetComments(id);
+    if (+id > 101 || Number.isNaN(+id)) {
+        router.push("/posts");
+    }
+    if (!data || isLoading || isFetching) {
+        return <SpinnerForClient />;
+    }
+    return <Catalog posts={data} id={id} />;
 };
