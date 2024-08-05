73045d502c2f85c4fc7e0f94af071a27b9293fa2
Alexey Yankov <yankovav.wm@gmail.com>
2024-08-05 09:55:11 +0300

    posts description fix without localstorage to remote from server

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,8 +1,7 @@
 import { CommentPage } from "@/features/commentPage/CommentPage";
-import NotFoundPage from "@/features/notFoundPage/NotFound";
 
 const Post = async ({ params }: { params: { id: string } }) => {
-  return <CommentPage id={params.id} />;
+  return <CommentPage id={params.id}/>;
 };
 
 export default Post;
--- src/features/catalog/catalog.tsx
+++ src/features/catalog/catalog.tsx
@@ -1,45 +1,49 @@
 "use client";
 
 import Image from "next/image";
-import { usePathname } from "next/navigation";
-import { CommentType, PostType } from "@/types/types";
-import { Center, List, Text } from "@chakra-ui/react";
-import { Post } from "../post";
+import {usePathname} from "next/navigation";
+import {CommentType, PostType} from "@/types/types";
+import {Center, List, Text} from "@chakra-ui/react";
+import {Post} from "../post";
+import {useGetCurrentPost} from "@/api/postsApi";
+import {SpinnerForClient} from "@/ui-kit/spinner";
 
 type CatalodType<T> = {
-  posts: Array<T>;
+    posts: Array<T>;
+    id?: string;
 };
 
 export const Catalog = (prop: CatalodType<CommentType | PostType>) => {
-  const pathname = usePathname();
-  const pathnameId = pathname.slice(7);
-  const commentOrPostType = !pathname.slice(6).length;
-  const getDescription = localStorage.getItem("description");
-  return (
-    <List w={"100%"}>
-      {commentOrPostType && (
-        <Text fontSize="2xl" pt="8">
-          Posts list:
-        </Text>
-      )}
-      {!commentOrPostType && (
-        <Center pt="12" position={"relative"}>
-          <Image
-            src={`https://via.assets.so/furniture.png?id=${pathnameId}&q=95&w=360&h=360&fit=fill`}
-            alt={"image-collection"}
-            height={182}
-            width={180}
-          />
-        </Center>
-      )}
-      {!commentOrPostType && <Text pt="6"> {getDescription}</Text>}
-      {prop.posts.length
-        ? prop.posts.map((post) => {
-            return (
-              <Post key={post.id} post={post} isPost={commentOrPostType} />
-            );
-          })
-        : null}
-    </List>
-  );
+    const {data, isFetching, isLoading} = useGetCurrentPost(prop.id ?? "");
+    const pathname = usePathname();
+    const pathnameId = pathname.slice(7);
+    const commentOrPostType = !pathname.slice(6).length;
+    if (isFetching || isLoading) {
+        return <SpinnerForClient />;
+    }
+    return (
+        <List w={"100%"}>
+            {commentOrPostType && (
+                <Text fontSize="2xl" pt="8">
+                    Posts list:
+                </Text>
+            )}
+            {!commentOrPostType && (
+                <Center pt="12" position={"relative"}>
+                    <Image
+                        src={`https://via.assets.so/furniture.png?id=${pathnameId}&q=95&w=360&h=360&fit=fill`}
+                        alt={"image-collection"}
+                        height={182}
+                        width={180}
+                    />
+                </Center>
+            )}
+            {!commentOrPostType && <Text pt="6"> {data?.body}</Text>}
+            {prop.posts.length
+                ? prop.posts.map((post) => {
+                      return <Post key={post.id} post={post} isPost={commentOrPostType} />;
+                  })
+                : null}
+        </List>
+    );
 };
