422936f2860b9af755eca139aa3069386794a176
Alexey Yankov <yankovav.wm@gmail.com>
2024-08-05 11:51:12 +0300

    post router fix for posts more 101

--- src/features/commentPage/CommentPage.tsx
+++ src/features/commentPage/CommentPage.tsx
@@ -3,10 +3,14 @@
 import { SpinnerForClient } from "@/ui-kit/spinner";
 import { Catalog } from "../catalog/catalog";
 import { useGetComments } from "@/api/commentsApi";
+import { useRouter } from "next/navigation";
 
 export const CommentPage = ({ id }: { id: string }) => {
+  const router = useRouter();
   const { data, isLoading, isFetching } = useGetComments(id);
-
+  if (+id > 101) {
+    router.push("/posts");
+  }
   if (!data || isLoading || isFetching) {
     return <SpinnerForClient />;
   }
